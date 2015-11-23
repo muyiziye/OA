cb808a159e1e5addf5ae014453a459cf5325d1ce
muyiziye <348610826@qq.com>
2015-11-23 22:54:27 +0800

    change structDef

---
+++ project_OA/structDef.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: structDef.h
+	> Author: muyiziye
+	> Mail: ahfyly@foxmail.com
+	> Created Time: 2015年11月23日 星期一 22时02分18秒
+ ************************************************************************/
+
+#ifndef _STRUCTDEF_H
+#define _STRUCTDEF_H
+#endif
+
