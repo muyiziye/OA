3ab6797116d9a9f045c734a6a3fa1587961d63d1
muyiziye <348610826@qq.com>
2015-11-23 21:42:09 +0800

    add OA code

---
+++ project_OA/main.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: muyiziye
+	> Mail: ahfyly@foxmail.com
+	> Created Time: 2015年11月23日 星期一 20时20分37秒
+ ************************************************************************/
+
+#include<stdio.h>
+#ifndef MENU_H
+#include"menu.h"
+#endif
+int main()
+{
+    printf("go into my OA\n");
+    showMenu();
+    return 1;
+}
---
+++ project_OA/menu.c
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: menu.c
+	> Author: muyiziye
+	> Mail: ahfyly@foxmail.com
+	> Created Time: 2015年11月23日 星期一 20时47分20秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include"menu.h"
+
+void showMenu()
+{
+    printf("/*********************/\n");
+    printf("\n");
+    printf("\n");
+    printf("    this is my OA\n");
+    printf("\n");
+    printf("\n");
+    printf("/*********************/\n");
+    return;
+}
