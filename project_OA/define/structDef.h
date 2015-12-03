b5e059d6d242bdccb9fbb79e9dec72f24a1cd3c1
muyiziye <348610826@qq.com>
2015-12-03 17:42:03 +0800

    change the file

---
+++ project_OA/define/structDef.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: structDef.h
+	> Author: muyiziye
+	> Mail: ahfyly@foxmail.com
+	> Created Time: 2015年11月23日 星期一 22时02分18秒
+ ************************************************************************/
+
+#ifndef _STRUCTDEF_H
+#define _STRUCTDEF_H
+typedef struct
+{
+    int day;
+    float breakFaseFee;
+    float lunchFee;
+    float dinnerFee;
+    float shoppingFee;
+    float otherFee;
+    float wage;
+    float otherEarn;
+}DEILYPAYEARN_T;
+
+#endif
+
---
+++ project_OA/menu/menu.c
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: menu.c
+	> Author: muyiziye
+	> Mail: ahfyly@foxmail.com
+	> Created Time: 2015年11月23日 星期一 20时47分20秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include"menu.h"
+
+void showMenu()
+{
+    // this function can clear screen
+    system("clear");
+printf("/******************************************************************************/\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("                                this is my OA\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+printf("/******************************************************************************/\n");
+    printf("pls input \'Enter\' to transfer other window\n");
+    getchar();
+    // this function can clear screen 
+    system("clear");
+    system("reset");
+    return;
+}
